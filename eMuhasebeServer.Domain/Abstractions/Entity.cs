d10e4d147150f37a735d8fc52c247eba3215e599
SavasDuzgun <savasdzgn@gmail.com>
2024-11-07 11:50:29 +0300

    domain katmanında Abstractions klasöründeki Entity class ı için açıklama yazıldı.

--- eMuhasebeServer.Domain/Abstractions/Entity.cs
+++ eMuhasebeServer.Domain/Abstractions/Entity.cs
@@ -1,5 +1,6 @@
 ﻿namespace eMuhasebeServer.Domain.Abstractions
 {
+    //AppUser haricinde başka entity eklenirse buradaki Entity class ından inherit ederek Id alanını guid yapıp constructor yani instance üretme esnasında id nin bir guid olarak otomatik oluşması sağlandı.
     public abstract class Entity
     {
         public Guid Id { get; set; }
