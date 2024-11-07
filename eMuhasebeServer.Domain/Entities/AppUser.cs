8ed4b2b2bb799c69baf1eb407f3ca12ec6e4d3d7
SavasDuzgun <savasdzgn@gmail.com>
2024-11-07 11:47:11 +0300

    Domain katmanında AppUser class ı için açıklama yazıldı.

--- eMuhasebeServer.Domain/Entities/AppUser.cs
+++ eMuhasebeServer.Domain/Entities/AppUser.cs
@@ -2,10 +2,13 @@
 
 namespace eMuhasebeServer.Domain.Entities
 {
-    public sealed class AppUser : IdentityUser<Guid>
+    //identity kütüphanesi kullanıldı. User için inherit edildi. İçinde Id,UserName,Email,Password var. Orada olmayan FirstName ve LastName eklendi.
+    public sealed class AppUser : IdentityUser<Guid> 
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        
+        //bu class new lendiğinde instance oluşturulurken tam adı veren FullName oluşturuldu. Db de kolon olarak görünmez.
         public string FullName => string.Join(" ", FirstName, LastName);
         public string? RefreshToken { get; set; }
         public DateTime? RefreshTokenExpires { get; set; }
