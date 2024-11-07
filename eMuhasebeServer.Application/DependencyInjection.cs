799960f4c56b11d59eccd626ccea0b658de2e850
SavasDuzgun <savasdzgn@gmail.com>
2024-11-07 11:53:44 +0300

    Application katmanı DI class ı için açıklama yazıldı.

--- eMuhasebeServer.Application/DependencyInjection.cs
+++ eMuhasebeServer.Application/DependencyInjection.cs
@@ -8,14 +8,17 @@ namespace eMuhasebeServer.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            //AutoMapper için DI
             services.AddAutoMapper(typeof(DependencyInjection).Assembly);
 
+            //CQRS pattern için eklenen MediaTR için DI, MediaTR eklenince behavior özelliği kullanılabiliyor.
             services.AddMediatR(conf =>
             {
                 conf.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly);
                 conf.AddOpenBehavior(typeof(ValidationBehavior<,>));
             });
 
+            //FluentValidation için DI
             services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
 
             return services;
