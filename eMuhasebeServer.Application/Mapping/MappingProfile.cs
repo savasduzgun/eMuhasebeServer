ce7efc21646b24bbc41241e27389ac7dbc3bf0c6
SavasDuzgun <savasdzgn@gmail.com>
2024-11-07 13:39:05 +0300

    MappigProfile için açıklama yazıldı.

--- eMuhasebeServer.Application/Mapping/MappingProfile.cs
+++ eMuhasebeServer.Application/Mapping/MappingProfile.cs
@@ -4,6 +4,7 @@ namespace eMuhasebeServer.Application.Mapping
 {
     public sealed class MappingProfile : Profile
     {
+        //AutoMapper için map leme yapılırsa bu alan kullanılacak.
         public MappingProfile()
         {
 
