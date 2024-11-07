16948578dd5876bacd7e067ee0cfcd975a27f303
SavasDuzgun <savasdzgn@gmail.com>
2024-11-07 13:44:36 +0300

    LoginCommand için açıklama yazıldı.

--- eMuhasebeServer.Application/Features/Auth/Login/LoginCommand.cs
+++ eMuhasebeServer.Application/Features/Auth/Login/LoginCommand.cs
@@ -3,6 +3,8 @@ using TS.Result;
 
 namespace eMuhasebeServer.Application.Features.Auth.Login
 {
+    //Giriş için kullanılacak request
+    //Geriye Result Pattern içerisinde döner.
     public sealed record LoginCommand(
         string EmailOrUserName,
         string Password) : IRequest<Result<LoginCommandResponse>>;
