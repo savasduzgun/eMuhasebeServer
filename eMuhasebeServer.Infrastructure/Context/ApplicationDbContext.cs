aff53db46cc512211168c77f58a2625a164a78a9
SavasDuzgun <savasdzgn@gmail.com>
2024-11-08 11:31:58 +0300

    ApplicationDbContext class ına açıklama yazıldı.

--- eMuhasebeServer.Infrastructure/Context/ApplicationDbContext.cs
+++ eMuhasebeServer.Infrastructure/Context/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace eMuhasebeServer.Infrastructure.Context
 {
+    //database bağlantısı EF kullanan ApplicationDbContext, identity kütüphanesini kullandığım için IdentityDbContext ekledim User ve role yapısı olmadığı için Role yapısı verildi. Kendi repository patternım dan gelen IUnitOfWork verdim. Kayıt işlemlerin unitofwork interface i kullanılacak.
     internal sealed class ApplicationDbContext : IdentityDbContext<AppUser, IdentityRole<Guid>, Guid>, IUnitOfWork
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
@@ -14,7 +15,11 @@ namespace eMuhasebeServer.Infrastructure.Context
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            //eklenen bir entity varsa entity nin ayarlarını bu dosyada değil ayrı bir dosyada yapmak tercih edildiği için IEntityTypeConfiguration etmişleri otomatik bulmasını ve buraya dahil etmesini söyleyen assembly verilen metod çağrıldı.
+
             builder.ApplyConfigurationsFromAssembly(typeof(DependencyInjection).Assembly);
+
+            //identity de kullanılmaycak classlar db de oluşmasın diye ignore edildi.
 
             builder.Ignore<IdentityUserLogin<Guid>>();
             builder.Ignore<IdentityRoleClaim<Guid>>();
