7c831a89012be448ae3651ccd2b24fa58421452f
savasduzgun <savasdzgn@gmail.com>
2024-11-08 17:19:04 +0300

    AuthController için açıklama yazıldı.

--- eMuhasebeServer.WebAPI/Controllers/AuthController.cs
+++ eMuhasebeServer.WebAPI/Controllers/AuthController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace eMuhasebeServer.WebAPI.Controllers
 {
-    [AllowAnonymous]
+    [AllowAnonymous] //kullanıcı kontrolü bu sayfada olamsın
     public sealed class AuthController : ApiController
     {
         public AuthController(IMediator mediator) : base(mediator)
@@ -16,6 +16,7 @@ namespace eMuhasebeServer.WebAPI.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginCommand request, CancellationToken cancellationToken)
         {
+            //bütün endpoitlerde kullanılacak standart yapı, response result olarak cevap alıyor, result ın içinde statüs code var başarılı başarısız dönebiliyor normal response da bir değer varsa onu yolluyor cevap olarak
             var response = await _mediator.Send(request, cancellationToken);
             return StatusCode(response.StatusCode, response);
         }
