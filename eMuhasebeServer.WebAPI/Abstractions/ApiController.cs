97fa1071a6eb82662c2ee8009309b99480cf2d85
SavasDuzgun <savasdzgn@gmail.com>
2024-11-08 12:10:56 +0300

    ApiController class ı için açıklama yazıldı.

--- eMuhasebeServer.WebAPI/Abstractions/ApiController.cs
+++ eMuhasebeServer.WebAPI/Abstractions/ApiController.cs
@@ -4,8 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace eMuhasebeServer.WebAPI.Abstractions
 {
+    //her controllerda kullanılan default yapı abstract class haline getirildi. mediatr kütüphanesi kullanıldığı için DI yapıldı.
     [Route("api/[controller]/[action]")]
     [ApiController]
+    //eklenilen her sayfa kullancı girişi yapılarak kullanılacak
     [Authorize(AuthenticationSchemes = "Bearer")]
     public abstract class ApiController : ControllerBase
     {
